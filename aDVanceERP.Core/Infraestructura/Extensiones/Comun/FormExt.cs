4d6bf2a5955dd420a2b95fe0d139a6752948d679
cdavisong <carlosdavison@nauta.cu>
2025-10-08 20:15:17 -0400

    Modificaciones en extensiones de formulario

--- aDVanceERP.Core/Infraestructura/Extensiones/Comun/FormExt.cs
+++ aDVanceERP.Core/Infraestructura/Extensiones/Comun/FormExt.cs
@@ -1,13 +1,12 @@
 ﻿namespace aDVanceERP.Core.Infraestructura.Extensiones.Comun;
 
 public static class FormExt {
-    public static IEnumerable<Control> GetAllControls(this Control container) {
-        foreach (Control control in container.Controls) {
+    public static IEnumerable<Control> GetAllControls(this Control parent) {
+        foreach (Control control in parent.Controls) {
             yield return control;
 
-            // Llamada recursiva para controles anidados
-            foreach (Control childControl in GetAllControls(control)) {
-                yield return childControl;
+            foreach (Control descendant in control.GetAllControls()) {
+                yield return descendant;
             }
         }
     }
